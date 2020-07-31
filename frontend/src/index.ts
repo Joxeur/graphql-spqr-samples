6a958bc4d14d5a3ad2073ca1d676ed3d3009d8e7
Jonas Lauber <jonas.lauber@aspect3.ch>
2020-07-31 08:21:35 +0200

    Made mutation with variables

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,13 +1,11 @@
 import { GraphQLClient } from 'graphql-request';
-import {getSdk, namedOperations} from './generated/graphql'; // THIS FILE IS THE GENERATED FILE
-
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import {getSdk} from './generated/graphql'; // THIS FILE IS THE GENERATED FILE
 
 async function main(): Promise<void> {
 	const client = new GraphQLClient('http://localhost:8000/graphql');
 	const sdk = getSdk(client);
 
-	const createResult = await sdk.createProjectAndTask();
+	const createResult = await sdk.createProjectAndTask({projectcode: 'ABAB'});
 	console.log(`GraphQL data:`, createResult);
 
 	const project = await sdk.findProject({code: createResult.createProject.code});
